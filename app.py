517780e52e8aa14ab89807c3d207eae6eb277b3a
yaqeen-03 <162414876+yaqeen-03@users.noreply.github.com>
2024-04-14 03:58:29 +0000

    ALL

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from database import load_projects_from_db, load_project_from_db, add_message_to_db
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-  return 'Hello, yaqeen!'
+  projects = load_projects_from_db()
+  return render_template('home.html',
+                         myskills=projects,
+                         myname='yaqeen adnan')
 
-if __name__ =="__main__":
-  app.run(host='0.0.0.0', port=8080) #run Flask server
+
+@app.route("/project/<id>")
+def show_project(id):
+  project = load_project_from_db(id)
+  return render_template('projectsdetails.html', project=project)
+
+@app.route("/contactme")
+def contactpage():
+  return render_template('contactme.html')
+
+@app.route("/message", methods=['post'])
+def message():
+  data = request.form
+  add_message_to_db(data)
+  return render_template('message_submitted.html', message=data)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=True)  #run flask serve
---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string)
+
+def load_projects_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from myprojects"))
+    myprojects = []
+    for row in result.all():
+      myprojects.append(dict(row._mapping))
+    return myprojects
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from myprojects where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+
+def add_message_to_db(data):
+  with engine.connect() as conn:
+    sql = text(
+        f"INSERT INTO messages (full_name,email,message) VALUES (\'{data['full_name']}\',\'{data['email']}\',\'{data['message']}\')"
+    )
+
+    conn.execute(sql)
+    conn.commit()
